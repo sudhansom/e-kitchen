e109dd233c897fbcc07a514723dc1a62facbff7b
bhuwan <bkspoudel@gmail.com>
2023-08-05 15:49:10 +0200

    fix color according to the amount

---
+++ src/app/data.ts
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    name: 'potato',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+  {
+    name: 'potato',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+  {
+    name: 'onion',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+  {
+    name: 'bread',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+  {
+    name: 'salt',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+  {
+    name: 'potato',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+  {
+    name: 'potato',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+  {
+    name: 'onion',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+  {
+    name: 'bread',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+  {
+    name: 'salt',
+    amount: 80,
+    image: '../assets/images/potato.png',
+  },
+]
--- src/app/views/root/root.component.ts
+++ src/app/views/root/root.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component} from "@angular/core";
-
+import { data } from "src/app/data";
 @Component ({
   selector: 'app-root',
   templateUrl: './root.component.html',
@@ -8,5 +8,23 @@ import { ChangeDetectionStrategy, Component} from "@angular/core";
 })
 
 export class RootComponent {
+
+  data = data.map(item => {
+    let borderColor = '';
+    if(item.amount > 75){
+      borderColor = 'green';
+    }else if(item.amount <= 75 && item.amount > 40){
+      borderColor = 'orange';
+    }
+    else if(item.amount <= 40 && item.amount > 10){
+      borderColor = 'purple';
+    }else {
+      borderColor = 'red';
+    }
+    return {
+      ...item,
+      color: borderColor,
+    }
+  });
 
 }
