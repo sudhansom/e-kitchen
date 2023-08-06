a92c06b413ce0f3e6bf2c5a08e4a54c3e5cbaa5d
bhuwan <bkspoudel@gmail.com>
2023-08-06 11:23:49 +0200

    get data from firebase

--- src/app/views/root/root.component.ts
+++ src/app/views/root/root.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, signal} from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal, OnInit} from "@angular/core";
+import { Data } from "@angular/router";
 import { data } from "src/app/data";
+import { DataService } from "src/app/services/data.service";
 
 interface IData {
   name: string,
@@ -15,29 +17,12 @@ interface IData {
   //changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class RootComponent {
+export class RootComponent implements OnInit {
 
   showAmount = signal(false);
   mySlider = 0;
 
-  data: IData[] = data.map(item => {
-    let borderColor = '';
-    if(item.amount > 75){
-      borderColor = 'green';
-    }else if(item.amount <= 75 && item.amount > 40){
-      borderColor = 'orange';
-    }
-    else if(item.amount <= 40 && item.amount > 10){
-      borderColor = 'purple';
-    }else {
-      borderColor = '#880808';
-    }
-    return {
-      ...item,
-      color: borderColor,
-      showProgress: false,
-    }
-  }).sort((a,b) => a.amount - b.amount);
+  data: IData[] = [];
 
   showProgressBar(name: string){
     this.data = this.data.map(item => {
@@ -69,6 +54,35 @@ export class RootComponent {
           ...item
         }
       }
+    })
+  }
+
+  constructor(private dataService: DataService){}
+
+  ngOnInit(): void {
+    this.dataService.getFormData().subscribe(data => {
+      let rawData = [];
+      for(let key in data){
+        rawData.push(data[key]);
+      }
+      this.data = rawData.map(item => {
+        let borderColor = '';
+        if(item.amount > 75){
+          borderColor = 'green';
+        }else if(item.amount <= 75 && item.amount > 40){
+          borderColor = 'orange';
+        }
+        else if(item.amount <= 40 && item.amount > 10){
+          borderColor = 'purple';
+        }else {
+          borderColor = '#880808';
+        }
+        return {
+          ...item,
+          color: borderColor,
+          showProgress: false,
+        }
+      }).sort((a,b) => a.amount - b.amount);
     })
   }
 
